9382c195efdec742c88e09b7331bb3ff9b682809
Lily W <lilycwei2027@gmail.com>
2025-04-15 21:13:12 -0400

    add player_movement_example

--- src/player_movement_example.c
+++ src/player_movement_example.c
@@ -1,70 +1,7 @@
 #include "assets/blob/blob.h"
 #include "example.h"
-#include "tonc.h"
+#include "player_movement.h"
 
-typedef struct object_t {
-  OBJ_ATTR *attr;
-  float x;
-  float y;
-  float x_velocity;
-  float y_velocity;
-  float x_acceleration;
-  float y_acceleration;
-  bool jumping;
-} Object;
-
-void set_obj_x(Object *obj, int x);
-void set_obj_y(Object *obj, int y);
-void set_obj_x_velocity(Object *obj, float x_velocity);
-void set_obj_y_velocity(Object *obj, float y_velocity);
-void set_obj_x_acceleration(Object *obj, float x_acceleration);
-void set_obj_y_acceleration(Object *obj, float y_acceleration);
-void set_jumping(Object *obj, bool jumping);
-void key_input(Object *obj);
-void update_physics(Object *obj);
-
-void set_obj_x(Object *obj, int x) {
-  obj->attr->attr1 = ATTR1_X(x) | ATTR1_SIZE_16x16;
-  obj->x = x;
-}
-
-void set_obj_y(Object *obj, int y) {
-  obj->attr->attr0 = ATTR0_Y(y) | ATTR0_SQUARE | ATTR0_4BPP | ATTR0_REG;
-  obj->y = y;
-}
-
-void set_obj_x_velocity(Object *obj, float x_velocity) {
-  obj->x_velocity = x_velocity;
-}
-
-void set_obj_y_velocity(Object *obj, float y_velocity) {
-  obj->y_velocity = y_velocity;
-}
-
-void set_obj_x_acceleration(Object *obj, float x_acceleration) {
-  obj->x_acceleration = x_acceleration;
-}
-
-void set_obj_y_acceleration(Object *obj, float y_acceleration) {
-  obj->y_acceleration = y_acceleration;
-}
-
-void set_jumping(Object *obj, bool jumping) { obj->jumping = jumping; }
-
-void key_input(Object *obj) {
-  key_poll();
-  if (!obj->jumping) {
-    if (key_is_down(KEY_UP)) {
-      set_obj_y_velocity(obj, -10);
-      set_jumping(obj, true);
-    }
-  }
-}
-
-void update_physics(Object *obj) {
-  set_obj_y_velocity(obj, obj->y_velocity += obj->y_acceleration);
-  set_obj_y(obj, obj->y += (obj->y_velocity + 0.5 * obj->y_acceleration));
-}
 
 int main(void) {
   // general idea - generate object way off the screen and as it moves over the
