3b2202cfa5dc4c8379a1a85a088ae23600d2d4ae
Lily W <lilycwei2027@gmail.com>
2025-05-04 16:22:12 -0400

    add test cases for cheat sprites and corrected bug in kinematics

--- test_include/test_object_utils.h
+++ test_include/test_object_utils.h
@@ -3,11 +3,25 @@
 #include <stdbool.h>
 
 /**
- * function: check_obj_offscreen
- * 
- * Tests to see if check_obj_offscreen returns a 1 for
- * the right field if an Object is offscreen to the right.
- * 
- * @return A 1 if the Object is completely offscreen to the right, otherwise a 0.
+ * Tests to see if toggling cheat mode off sets all sprites to the default
+ * sprite.
+ *
+ * In this test, both the player and obstacle have their default sprite set to
+ * DINO. Therefore, their sprite id's should remain as DINO.
+ *
+ * @return 1 if both the player and obstacle sprite IDs are the default, 0
+ * otherwise.
  */
-int test_right_offscreen();
+int test_toggle_cheat_state_off();
+
+/**
+ * Tests to see if toggling cheat mode on sets all sprites to the cheat mode
+ * sprite.
+ *
+ * In this test, both the player and obstacle have their default sprite set to
+ * DINO. Therefore, their sprite id's should become BIPLUP.
+ *
+ * @return 1 if both the player and obstacle sprite IDs are the the cheat mode
+ * sprite, 0 otherwise.
+ */
+int test_toggle_cheat_state_on();
--- tests/test_object_utils.c
+++ tests/test_object_utils.c
@@ -1,19 +1,50 @@
 #include "test_object_utils.h"
 
+#include "kinematics.h"
 #include "object_utils.h"
+#include "system_utils.h"
 #include "tonc.h"
 #include "types.h"
 #include <stdbool.h>
 
 #include "assets/blob.h"
 
-int test_right_offscreen() {
-  Object *object = &(Object){};
-  float offscreen_x = 240 + 32 + 1;
-  RECT direction;
-  object_constructor(object, 1, offscreen_x, 10.0, BLOB);
+// object constructor variables
+int test_object_counter = 0;
+float test_x = 0.0;
+float test_y = 0.0;
+Sprite_ID test_tile_number = DINO;
+extern int cheat_sprite_state;
 
-  check_obj_offscreen(object, &direction);
+// obstacle constructor variables
+float test_x_velocity = 1.0;
+int test_frame_threshold = 1;
 
-  return direction.right == 1;
+int test_toggle_cheat_state_on() {
+  // cheat_sprite_state starts off as off (0) as the cheat_toggle_pokemon will
+  // toggle it on
+  cheat_sprite_state = 0;
+
+  // create test obstacle
+  // note: test_obs will have the default sprite id of DINO
+  Obstacle *test_obs = &(Obstacle){0};
+  test_obs->obj_args = &(Object){0};
+
+  obstacle_constructor(test_obs, test_object_counter, test_y, test_x_velocity,
+                       test_frame_threshold, test_tile_number);
+
+  Obstacle *test_obstacles[1] = {test_obs};
+
+  // create test player
+  // note: test_player will have the default sprite id of DINO
+  Player *test_player = &(Player){0};
+  test_player->obj_args = &(Object){0};
+  player_constructor(test_player);
+
+  cheat_toggle_pokemon(test_player, test_obstacles);
+
+  // cheat_toggle_pokemon should set both test_player and test_obs sprite id to
+  // BIPLUP
+  return (get_sprite_id(test_player->obj_args) == BIPLUP) &&
+         (get_sprite_id(test_obs->obj_args) == BIPLUP);
 }
