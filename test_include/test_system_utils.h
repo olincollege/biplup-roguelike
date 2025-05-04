bd24870a528cde402cb7ba3d4f8f36212e029b8e
ram <ramzey.burdette2005@gmail.com>
2025-05-04 16:22:26 -0400

    added system_utils tests. all passed

---
+++ test_include/test_system_utils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int test_init_main(void);
+
+int test_reset_game_state(void);
+
+int test_retrieve_high_score(void);
+
+// int test_save_high_score(void);
+
+// int test_ignore_low_score(void);
